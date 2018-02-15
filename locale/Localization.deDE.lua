5d47f6b89cb2a65243dbc77d2b613dfb63630127
André Evertz <andre.evertz@devil-coding.de>
2018-02-15 23:20:48 +0100

    Updated translation files

--- locale/Localization.deDE.lua
+++ locale/Localization.deDE.lua
@@ -2,3 +2,5 @@ local L = LibStub('AceLocale-3.0'):NewLocale('FarmBuddy', 'deDE', false)
 if not L then return end
 
 L = L or {}
+L["FARM_BUDDY_ANCHOR_HELP_TEXT"] = "Linke Maustaste halten zum bewegen. Rechtsklick zum schließen."
+L["FARM_BUDDY_NOTIFICATION_DEMO_ITEM_NAME"] = "Ruhestein"
--- locale/Localization.koKR.lua
+++ locale/Localization.koKR.lua
@@ -2,3 +2,7 @@ local L = LibStub('AceLocale-3.0'):NewLocale('FarmBuddy', 'koKR', false)
 if not L then return end
 
 L = L or {}
+--Translation missing 
+L["FARM_BUDDY_ANCHOR_HELP_TEXT"] = "Hold left mouse button to move. Right click to close."
+--Translation missing 
+L["FARM_BUDDY_NOTIFICATION_DEMO_ITEM_NAME"] = "Hearthstone"
